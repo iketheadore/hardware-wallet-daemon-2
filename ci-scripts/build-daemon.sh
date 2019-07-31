f93cd57231ef274a79d6ba510f7a5ad1733ae479
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-07-31 17:37:26 +1000

    Use $OSTYPE to differ OS
    Otherwise it can only be used on travis servers

--- ci-scripts/build-daemon.sh
+++ ci-scripts/build-daemon.sh
@@ -4,9 +4,9 @@ set -e -o pipefail
 echo "start to build daemon..."
 pushd "build"
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then ./build-daemon-release.sh  'windows/386,windows/amd64,linux/386,linux/amd64,linux/arm-7' ;fi
+if [[ "$OSTYPE" == "linux"* ]]; then ./build-daemon-release.sh  'windows/386,windows/amd64,linux/386,linux/amd64,linux/arm-7' ;fi
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "load osx config"
 	. build-conf.sh "darwin-10.10/amd64"
 	OSX64="${DMN_OUTPUT_DIR}/${OSX64_DMN}"
